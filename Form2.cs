b6bebbbdeb56290051417778f8082067ba901e90
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-25 14:51:00 +0300

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
 using OpenCvSharp;
@@ -17,38 +16,18 @@ namespace bot
         private void button1_Click(object sender, EventArgs e)
         {
             Thread.Sleep(2000);
-
-            var process = Process.GetCurrentProcess();
-            WorkWithProcess.BringProcessWindowToFront(process);
-            var windowHandle = process.MainWindowHandle;
-
-            // Захватываем окно игры
-            Bitmap screenCapture = CaptureScreen.CaptureWindow(windowHandle);
-            Bitmap template = new Bitmap(@"..\..\..\Images\templateOfHealthBar.png");
-
-            if (WorkWithImages.IsImageMatchWithTemplate(screenCapture, template))
-            {
-                MessageBox.Show("Шаблон найден!");
-            }
-            else
-            {
-                MessageBox.Show("Шаблон НЕ найден.");
-            }
+            MessageBox.Show("Тестовая кнопка нажата");
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
             var gkh = new GlobalKeyboardHook();
             gkh.HookedKeys.Add(Keys.A);
-            gkh.HookedKeys.Add(Keys.B);
-            gkh.KeyUp += Gkh_KeyUp;
-            gkh.Hook();
-        }
-
-        private void Gkh_KeyUp(object sender, KeyEventArgs e)
-        {
-            Console.WriteLine("Клавиша нажата в Form2");
-            e.Handled = true;
+            gkh.KeyUp += (s, ev) =>
+            {
+                Console.WriteLine("A нажата");
+                ev.Handled = true;
+            };
         }
     }
 }
