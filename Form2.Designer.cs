757f162b54817bdd7e686013eb7d1c9d910a88d8
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-25 14:50:11 +0300

    Update Form2.Designer.cs

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -1,14 +1,14 @@
+using System;
 using System.ComponentModel;
 using System.Windows.Forms;
-using System.Drawing;
 
 namespace bot
 {
     partial class Form2
     {
         private IContainer components = null;
-        private Button button1;
         private CheckBox checkBox1;
+        private Button button1;
         private Label label1;
 
         protected override void Dispose(bool disposing)
@@ -30,9 +30,9 @@ namespace bot
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new Point(157, 12);
+            this.checkBox1.Location = new System.Drawing.Point(157, 12);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 0;
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -40,9 +40,9 @@ namespace bot
             // 
             // button1
             // 
-            this.button1.Location = new Point(347, 276);
+            this.button1.Location = new System.Drawing.Point(347, 276);
             this.button1.Name = "button1";
-            this.button1.Size = new Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Test";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,24 +52,24 @@ namespace bot
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new Point(233, 146);
+            this.label1.Location = new System.Drawing.Point(233, 146);
             this.label1.Name = "label1";
-            this.label1.Size = new Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
 
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(800, 450);
-            this.Controls.Add(label1);
-            this.Controls.Add(button1);
-            this.Controls.Add(checkBox1);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
             this.Name = "Form2";
             this.Text = "Form2";
-            this.Load += new EventHandler(this.Form2_Load);
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
@@ -82,9 +82,5 @@ namespace bot
         }
 
         #endregion
-
-        private CheckBox checkBox1;
-        private Button button1;
-        private Label label1;
     }
 }
