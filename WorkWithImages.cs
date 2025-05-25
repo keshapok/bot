92ed500123d516e42bd8a6f5ad90c351ad0861aa
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-25 13:51:53 +0300

    Update WorkWithImages.cs

--- WorkWithImages.cs
+++ WorkWithImages.cs
@@ -1,16 +1,19 @@
-using System;
 using System.Drawing;
-using Point = System.Drawing.Point; // Явно указываем, какой Point использовать
+using OpenCvSharp;
 using OpenCvSharp.Extensions;
-using System.Diagnostics;
 using System.Threading.Tasks;
-using System.Linq;
 using System.Threading;
 
 namespace bot
 {
     class WorkWithImages
     {
+        public static Bitmap BringProcessToFrontAndCaptureGDIWindow(Process process)
+        {
+            WorkWithProcess.BringProcessWindowToFront(process);
+            return CaptureScreen.CaptureWindow(process.MainWindowHandle);
+        }
+
         public static Mat GetDiffInTwoImages(Bitmap firstState, Bitmap secondState)
         {
             using (Mat img1 = firstState.ToMat())
@@ -18,19 +21,13 @@ namespace bot
             using (Mat diff = new Mat())
             {
                 Cv2.Absdiff(img1, img2, diff);
-                Mat gray = new Mat();
-                Cv2.CvtColor(diff, gray, ColorConversionCodes.BGR2GRAY);
-                Mat thresholded = new Mat();
-                Cv2.Threshold(gray, thresholded, 70, 255, ThresholdTypes.Binary);
-                return thresholded;
-            }
-        }
 
-        public static Point GetBiggestCountourCoordinates(Point[][] pointsOfCountours)
-        {
-            if (pointsOfCountours.Length == 0) return new Point(622, 401);
-            var biggestContour = pointsOfCountours.OrderByDescending(c => c.Length).First();
-            return biggestContour[biggestContour.Length / 2];
+                // Пример создания маски
+                Mat mask = new Mat();
+                Cv2.CvtColor(diff, mask, ColorConversionCodes.BGR2GRAY);
+                Cv2.Threshold(mask, mask, 70, 255, ThresholdTypes.Binary);
+                return mask;
+            }
         }
 
         public static Point[][] FindCountoursAtImage(Mat image)
@@ -39,26 +36,28 @@ namespace bot
             return contours;
         }
 
+        public static Point GetBiggestCountourCoordinates(Point[][] pointsOfCountours)
+        {
+            if (pointsOfCountours.Length == 0)
+                return new Point(622, 401);
+
+            var biggestContour = pointsOfCountours.OrderByDescending(c => c.Length).First();
+            return biggestContour[biggestContour.Length / 2];
+        }
+
         public static bool IsImageMatchWithTemplate(Bitmap monsterRef, Bitmap monsterTemplate)
         {
             using (Mat reference = monsterRef.ToMat())
             using (Mat template = monsterTemplate.ToMat())
             using (Mat result = new Mat())
             {
-                Cv2.MatchTemplate(reference.CvtColor(ColorConversionCodes.BGR2GRAY),
-                                 template.CvtColor(ColorConversionCodes.BGR2GRAY),
-                                 result,
-                                 TemplateMatchModes.CCoeffNormed);
+                Cv2.MatchTemplate(reference.CvtColor(ColorConversionCodes.BGR2GRAY), 
+                                 template.CvtColor(ColorConversionCodes.BGR2GRAY), 
+                                 result, TemplateMatchModes.CCoeffNormed);
                 Cv2.Threshold(result, result, 0.7, 1.0, ThresholdTypes.Tozero);
                 Cv2.MinMaxLoc(result, out _, out double maxVal, out _, out _);
                 return maxVal >= 0.7;
             }
-        }
-
-        public static Bitmap BringProcessToFrontAndCaptureGDIWindow(Process process)
-        {
-            WorkWithProcess.BringProcessWindowToFront(process);
-            return CaptureScreen.CaptureWindow(process.MainWindowHandle);
         }
     }
 }
