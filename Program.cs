40005a7d1e4cb9fdfc327865574ea14e9f3e1fa4
keshapok <165573971+keshapok@users.noreply.github.com>
2025-05-25 18:00:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ namespace bot
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
